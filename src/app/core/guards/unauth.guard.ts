aa278ceab9e3744bab91867daba9f4a29317f5f5
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-08-02 18:43:30 -0300

    Create auth and unauth guards

---
+++ src/app/core/guards/unauth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpService } from 'src/app/core/services/http.service'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnauthGuard implements CanActivate {
+  constructor(
+    private httpService: HttpService,
+    private router: Router
+  ){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    let auth = this.httpService.checkAuth();
+
+    if (auth)
+      this.router.navigate(['dashboard']);
+
+    return !auth;
+  }
+}
