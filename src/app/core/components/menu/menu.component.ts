8266a245c4c1405e74388fc5df16e3d4cabcefa0
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-08-24 18:28:41 -0300

    Insert height 100% in router wrapper for vertical alignment

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-// Temp tests
-  import { HttpService } from 'src/app/core/services/http.service';
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -10,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {  
   constructor(
-    // Temp tests
-      public httpService: HttpService
   ) { }
 
   ngOnInit(): void {
